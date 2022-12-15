7523105574c9bdc1771dce705beae57458cd4ce2
Kshitij Joshi <iamkshitij5@gmail.com>
2022-12-15 23:26:39 +0530

    add buffers for later change, create two func for two different skits

--- Sound_app.py
+++ Sound_app.py
@@ -5,13 +5,19 @@ def main():
 	window = tk.Tk()
 	window.geometry("500x500")
 	window.title('sound_manager')
-	module1 = sound_module.Sound_module('sound_files/police_siren.mp3', window)
-	module2 = sound_module.Sound_module('sound_files/background_edited.mp3', window)
-	module3 = sound_module.Sound_module('sound_files/Ambulance_siren.mp3', window)
-	module4 = sound_module.Sound_module('sound_files/nokia_ringtone_edited.mp3', window)
-	module5 = sound_module.Sound_module('sound_files/land_line.mp3', window)
-	module6 = sound_module.Sound_module('sound_files/pakhavaj_1.mp3', window)
-	module7 = sound_module.Sound_module('sound_files/painful_beep.mp3', window)
+	def type_one():
+		module1 = sound_module.Sound_module('sound_files/police_siren.mp3', window)
+		module2 = sound_module.Sound_module('sound_files/background_edited.mp3', window)
+		module3 = sound_module.Sound_module('sound_files/Ambulance_siren.mp3', window)
+		module4 = sound_module.Sound_module('sound_files/nokia_ringtone_edited.mp3', window)
+		module5 = sound_module.Sound_module('sound_files/land_line.mp3', window)
+		module6 = sound_module.Sound_module('sound_files/pakhavaj_1.mp3', window)
+		module7 = sound_module.Sound_module('sound_files/painful_beep.mp3', window)
+		module8 = sound_module.Sound_module('sound_files/buffer_1.mp3', window)
+		module9 = sound_module.Sound_module('sound_files/buffer_2.mp3', window)
+		module10 = sound_module.Sound_module('sound_files/buffer_3.mp3', window)
+	def type_two():
+		pass
 	window.mainloop()
 if __name__=='__main__':
 	main()
