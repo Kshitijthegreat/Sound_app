3e03b118c48e3ac3adf6e2e4e0a47c21b0cae23c
Kshitij Joshi <iamkshitij5@gmail.com>
2022-12-15 23:47:35 +0530

    working condition, added init selector

--- Sound_app.py
+++ Sound_app.py
@@ -5,7 +5,7 @@ def main():
 	window = tk.Tk()
 	window.geometry("500x500")
 	window.title('sound_manager')
-	def type_one():
+	def init_one():
 		module1 = sound_module.Sound_module('sound_files/police_siren.mp3', window)
 		module2 = sound_module.Sound_module('sound_files/background_edited.mp3', window)
 		module3 = sound_module.Sound_module('sound_files/Ambulance_siren.mp3', window)
@@ -16,9 +16,11 @@ def main():
 		module8 = sound_module.Sound_module('sound_files/buffer_1.mp3', window)
 		module9 = sound_module.Sound_module('sound_files/buffer_2.mp3', window)
 		module10 = sound_module.Sound_module('sound_files/buffer_3.mp3', window)
-	def type_two():
+	def init_two():
 		pass
-	type_one()
+	inp = int(input('1->Godva\n2->natak_2\n>'))
+	inits = [init_one, init_two]
+	(inits[inp-1])()
 	window.mainloop()
 if __name__=='__main__':
 	main()
