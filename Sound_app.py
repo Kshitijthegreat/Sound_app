bf7c83f2a1987c4dba3d0f31ae812e23e39da3b3
Kshitij Joshi <iamkshitij5@gmail.com>
2022-12-15 23:20:59 +0530

    changed thing, works good

--- Sound_app.py
+++ Sound_app.py
@@ -1,8 +1,17 @@
-import tkinter as tk
-import sound_module
+def main():
+	import tkinter as tk
+	import sound_module
 
-window = tk.Tk()
-window.geometry("500x500")
-window.title('sound_manager')
-
-window.mainloop()
+	window = tk.Tk()
+	window.geometry("500x500")
+	window.title('sound_manager')
+	module1 = sound_module.Sound_module('sound_files/police_siren.mp3', window)
+	module2 = sound_module.Sound_module('sound_files/background_edited.mp3', window)
+	module3 = sound_module.Sound_module('sound_files/Ambulance_siren.mp3', window)
+	module4 = sound_module.Sound_module('sound_files/nokia_ringtone_edited.mp3', window)
+	module5 = sound_module.Sound_module('sound_files/land_line.mp3', window)
+	module6 = sound_module.Sound_module('sound_files/pakhavaj_1.mp3', window)
+	module7 = sound_module.Sound_module('sound_files/painful_beep.mp3', window)
+	window.mainloop()
+if __name__=='__main__':
+	main()
--- sound_module.py
+++ sound_module.py
@@ -1,5 +1,32 @@
 from just_playback import Playback
-class sound_module(Playback):
-	def __init__(self, file_name):
-		super.__init__()
-		self.load_file(filename)
+import tkinter as tk
+class Sound_module():
+	def __init__(self, file_name, window_arg, loop=True):
+		self.audio = Playback(file_name)
+		self.audio.loop_at_end(loop)
+		self.audio.set_volume(1)
+		self.window = window_arg
+		self.frame = tk.Frame(self.window)
+		self.slider = tk.Scale(self.frame, from_=0, to=100, orient = tk.HORIZONTAL, label = file_name, command = self.volume_update)
+		self.slider.set(100)
+		self.slider.pack(side = tk.LEFT)
+		self.button = tk.Button(self.frame, text = 'play/pause', command = self.button_func)
+		self.button.pack(side = tk.RIGHT)
+		self.text = tk.Text(self.frame, height =1, width=20)
+		self.text.pack(side = tk.BOTTOM)
+		self.frame.pack(side = tk.TOP)
+		self.update_text()
+	def button_func(self):
+		if self.audio.playing:
+			self.audio.pause()
+		else:
+			self.audio.play()
+	def volume_update(self, value=None):
+		val = (self.slider.get())/100
+		self.audio.set_volume(val)
+	def update_text(self):
+		self.text.delete("1.0", "end")
+		self.text.insert(tk.END, f'{self.audio.playing}->{round(self.audio.curr_pos)}')
+		self.text.after(1, self.update_text)
+
+
