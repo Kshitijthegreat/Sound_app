b22004599a39e8bcbe463a3bbe904aa0bfae5e40
Kshitij Joshi <iamkshitij5@gmail.com>
2022-12-21 19:36:50 +0530

    sound files updates

--- Sound_app.py
+++ Sound_app.py
@@ -8,24 +8,26 @@ def main():
 
 	def init_one():
 
-		module1 = sound_module.Sound_module('sound_files/police_siren.mp3', window)
-		module2 = sound_module.Sound_module('sound_files/background_edited.mp3', window)
+		module1 = sound_module.Sound_module('sound_files/one/police_siren.mp3', window)
+		module2 = sound_module.Sound_module('sound_files/one/background_edited.mp3', window)
 		#announcement
 		#module13 = sound_module.Sound_module('sound_files/recorded_faster.mp3', window, loop=False)
-		module3 = sound_module.Sound_module('sound_files/Ambulance_siren.mp3', window)
-		module11 = sound_module.Sound_module('sound_files/tick_tick.mp3', window)
-		module4 = sound_module.Sound_module('sound_files/nokia_ringtone_edited.mp3', window)
-		module5 = sound_module.Sound_module('sound_files/land_line.mp3', window)
-		module6 = sound_module.Sound_module('sound_files/pakhavaj_1.mp3', window)
-		module7 = sound_module.Sound_module('sound_files/painful_beep.mp3', window)
-		module8 = sound_module.Sound_module('sound_files/prakash_bolat_hota.mp3', window, loop=False)
-		module9 = sound_module.Sound_module('sound_files/recorded_original.mp3', window, loop=False)
-		module10 = sound_module.Sound_module('sound_files/recorded_fast(12x).mp3', window, loop=False)
-		module12 = sound_module.Sound_module('sound_files/recorded_faster(13x).mp3', window, loop=False)
+		module3 = sound_module.Sound_module('sound_files/one/Ambulance_siren.mp3', window)
+		module11 = sound_module.Sound_module('sound_files/one/tick_tick.mp3', window)
+		module4 = sound_module.Sound_module('sound_files/one/nokia_ringtone_edited.mp3', window)
+		module5 = sound_module.Sound_module('sound_files/one/land_line.mp3', window)
+		module6 = sound_module.Sound_module('sound_files/one/pakhavaj_1.mp3', window)
+		module7 = sound_module.Sound_module('sound_files/one/painful_beep.mp3', window)
+		module8 = sound_module.Sound_module('sound_files/one/prakash_bolat_hota.mp3', window, loop=False)
+		module9 = sound_module.Sound_module('sound_files/one/recorded_original.mp3', window, loop=False)
+		module10 = sound_module.Sound_module('sound_files/one/recorded_fast(12x).mp3', window, loop=False)
+		module12 = sound_module.Sound_module('sound_files/one/recorded_faster(13x).mp3', window, loop=False)
 	
 	def init_two():
-
-		pass
+		module1 = sound_module.Sound_module('sound_files/two/chipndale.mp3', window)
+		module2 = sound_module.Sound_module('sound_files/two/flute.mp3', window, loop=False)
+		module3 = sound_module.Sound_module('sound_files/two/gotya_title.mp3', window, loop=False)
+		module4 = sound_module.Sound_module('sound_files/two/Teen_deviya.mp3', window, loop=False)
 
 	inits = [init_one, init_two]
 	try:
