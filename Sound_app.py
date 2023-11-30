76caf3817a4682d7647e2006c64f38f7b8190d02
Kshitij Joshi <iamkshitij5@gmail.com>
2023-11-30 19:41:21 +0530

    added new tracks to program

--- Sound_app.py
+++ Sound_app.py
@@ -8,8 +8,10 @@ def main():
 
 	def init_one():
 		pass
-		#module1 = sound_module.Sound_module('sound_files/one/police_siren.mp3', window)
-		#module2 = sound_module.Sound_module('sound_files/one/background_edited.mp3', window)
+		module1 = sound_module.Sound_module('sound_files/edited/mayat/gauravSangitache.mp3', window)
+		module2 = sound_module.Sound_module('sound_files/edited/mayat/oDuniyaKeRakhwale.mp3', window)
+		module3 = sound_module.Sound_module('sound_files/edited/tavalAshram/googlePaySuccessSound.mp3', window, loop=False)
+		module4 = sound_module.Sound_module('sound_files/edited/tavalAshram/SpiritualMusic.mp3', window)
 		#announcement
 		#module13 = sound_module.Sound_module('sound_files/recorded_faster.mp3', window, loop=False)
 		#module3 = sound_module.Sound_module('sound_files/one/Ambulance_siren.mp3', window)
@@ -39,6 +41,7 @@ def main():
 	#	(inits[inp-1])()
 	#except:
 	#	(inits[0])()
+	init_one()
 	window.mainloop()
 
 if __name__=='__main__':
