53e7e2b3639c37508017aad9e95112f5c400b59e
Kshitij Joshi <iamkshitij5@gmail.com>
2023-12-10 18:30:40 +0530

    add vertically scrolled frame method

--- sound_module.py
+++ sound_module.py
@@ -102,5 +102,41 @@ class Divider():
 		self.name_label.pack(side=tk.TOP, padx=0, pady=0)
 	
 
-		
+class VerticalScrolledFrame(tk.Frame):
+		def __init__(self, parent, *args, **kw):
+			tk.Frame.__init__(self, parent, *args, **kw)
+
+			# Create a canvas object and a vertical scrollbar for scrolling it.
+			vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+			vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+			canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                           yscrollcommand=vscrollbar.set)
+			canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+			vscrollbar.config(command=canvas.yview)
+
+			# Reset the view
+			canvas.xview_moveto(0)
+			canvas.yview_moveto(0)
+
+			# Create a frame inside the canvas which will be scrolled with it.
+			self.interior = interior = tk.Frame(canvas)
+			interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=tk.NW)
+
+			# Track changes to the canvas and frame width and sync them,
+			# also updating the scrollbar.
+			def _configure_interior(event):
+				# Update the scrollbars to match the size of the inner frame.
+				size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+				canvas.config(scrollregion="0 0 %s %s" % size)
+				if interior.winfo_reqwidth() != canvas.winfo_width():
+					# Update the canvas's width to fit the inner frame.
+					canvas.config(width=interior.winfo_reqwidth())
+			interior.bind('<Configure>', _configure_interior)
+
+			def _configure_canvas(event):
+				if interior.winfo_reqwidth() != canvas.winfo_width():
+					# Update the inner frame's width to fill the canvas.
+					canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+			canvas.bind('<Configure>', _configure_canvas)		
 		
