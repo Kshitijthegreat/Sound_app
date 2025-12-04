15ba510212535c0e16095692c096f19dad949ae8
Kshitijthegreat <iamkshitij5@gmail.com>
2025-12-04 12:28:26 +0530

    add class soundFile add class unitFile

--- Sound_app.py
+++ Sound_app.py
@@ -1,5 +1,21 @@
 import tkinter as tk
 import sound_module
+
+
+class soundFile:
+	def __init__(self, str):
+		self.path = (str.split(","))[0]
+		self.loop = ( (str.split(","))[1] == "1" )
+
+class unitFile:
+	def __init__(self, str):
+		split_str = str.split("\n")
+		self.soundFiles = []
+		self.unitName=split_str[0]
+		for i in range(1,len(split_str)):
+			self.soundFiles.append(soundFile(split_str[i]))
+
+
 def main():
 	#use window as frame interior because legacy
 	main_window = tk.Tk()
@@ -30,3 +46,11 @@ def main():
 	#init_two()...
 
 	window.mainloop()
+
+#with open('fileSave.txt') as f: F_txt=f.read()
+#F_unitSplit=F_txt.split("::")
+
+if __name__=="__main__":
+	
+	uf = unitFile("name\npath/to/file1.mp3,1\npath/to/file2.mp3,0")
+	print(uf.unitName, uf.soundFiles[0].path, uf.soundFiles[0].loop)
